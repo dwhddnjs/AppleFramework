1ec738d05fc25199ff74b7f9a3b7e35346f09677
dwhddnjs <per1215@naver.com>
2024-06-07 21:28:38 +0900

    Initial Commit

---
+++ AppleFramework/AppleFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkApp.swift
+//  AppleFramework
+//
+//  Created by 이종원 on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFramework/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleFramework
+//
+//  Created by 이종원 on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
